e466f563141abb7d480a1747267455a0c7354446
Russell Koons <rkoons@gmail.com>
2018-11-29 09:58:59 -0600

    added a bunch of js

--- main.js
+++ main.js
@@ -6,16 +6,30 @@ const secret = 'b9f3a060ae7f8dde02e6dca7708f9dea'
 // 1. We need to call to the last.fm API and pull the top artists from tag that the user submits
 
   // First we handle the form to keep it from submitting nothing and we pull the value from the search
-function createUrl(tag) {
-  console.log(tag);
+
+function createUrl(params) {
+  const URL = `http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=${params.tag}&page=${params.page}&key=${params.key}&format=json`
+  console.log(URL);
+  console.log('createUrl working');
+}
+
+function createParams(genre, apiKey) {
+let pageNo = Math.floor(Math.random(1, 4) * Math.floor(4)) + 1;
+const params = {
+  key: apiKey,
+  tag: genre,
+  page: pageNo
+}
+console.log(params);
+createUrl(params);
+console.log(`createParams working`);
 }
 
 function handleForm() {
   $(`#js-submit`).click(event => {
     event.preventDefault();
     const genre = $(`#genre`).val();
-    createUrl(genre);
-    console.log(genre);
+    createParams(genre, apiKey);
   })
   console.log('handleForm working');
 }
@@ -26,9 +40,5 @@ function handleForm() {
 // 2. After that we need to choose a random band from the list and push it to the DOM
 
 // 3. Call the youtube API with a search for videos from the artist
-
-function handleForm() {
-  console.log('handleForm working');
-}
 
 $(handleForm());
