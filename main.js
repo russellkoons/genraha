4b86489bf371209db175bdbccc441c8d63475ab0
Russell Koons <rkoons@gmail.com>
2018-11-29 08:57:39 -0600

    fixed some stuff

--- main.js
+++ main.js
@@ -6,18 +6,21 @@ const secret = 'b9f3a060ae7f8dde02e6dca7708f9dea'
 // 1. We need to call to the last.fm API and pull the top artists from tag that the user submits
 
   // First we handle the form to keep it from submitting nothing and we pull the value from the search
-  function handleForm() {
-    $(`#js-submit`).click(event => {
-      event.preventDefault();
-      const tag=$(`#genre`).val();
-      createUrl(tag);
-    })
-    console.log('handleForm working');
-  }
+function createUrl(tag) {
+  console.log(tag);
+}
+
+function handleForm() {
+  $(`#js-submit`).click(event => {
+    event.preventDefault();
+    const genre = $(`#genre`).val();
+    createUrl(genre);
+    console.log(genre);
+  })
+  console.log('handleForm working');
+}
   // Then we take the search value and push it into a URL that calls the api
-  function createUrl(tag) {
-    console.log(`createUrl working`);
-  }
+
   // Then we call the API to get a JSON response
 
 // 2. After that we need to choose a random band from the list and push it to the DOM
